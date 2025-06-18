92689f84be33a5b2c5dbcca38af20f67ea65468f
Aniket Singh Rajput <rajnishsingh333u@gmail.com>
2025-06-18 03:48:15 +0000

    give me security rule and admin email is aniketsingh333u@gmail.com passw

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Added
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -25,5 +24,6 @@ if (!getApps().length) {
 }
 
 const auth = getAuth(app);
+const db = getFirestore(app); // Added Firestore instance
 
-export { app, auth };
+export { app, auth, db }; // Export db
