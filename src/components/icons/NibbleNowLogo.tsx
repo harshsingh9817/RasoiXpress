347f3225bbe83fb3134de7f601d2f8bd264e2c27
Aniket Singh Rajput <rajnishsingh333u@gmail.com>
2025-06-20 18:05:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <NibbleNowLogo>
    
    R

--- src/components/icons/NibbleNowLogo.tsx
+++ src/components/icons/NibbleNowLogo.tsx
@@ -5,21 +5,19 @@ const NibbleNowLogo: FC = () => {
   return (
     <div className="flex items-center" aria-label="NibbleNow logo mark">
       <svg
-        className="h-10 w-10" /* Increased size, removed mr-2 */
+        className="h-10 w-10"
         viewBox="0 0 100 100"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
       >
-        <rect width="100" height="100" rx="15" fill="hsl(var(--card))" /> {/* Background for the icon */}
-        {/* Abstract 'N' with a "nibble" or dynamic element */}
+        <rect width="100" height="100" rx="15" fill="hsl(var(--card))" />
         <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M20 80V20H35L60 55V20H75V80H60L35 45V80H20ZM68 80C72.4183 80 76 76.4183 76 72C76 67.5817 72.4183 64 68 64C63.5817 64 60 67.5817 60 72C60 76.4183 63.5817 80 68 80Z"
-          fill="hsl(var(--primary))" /* Color for the 'N' and circle */
+          d="M20 15 H38 V85 H20 Z M38 15 L62 85 L73 85 L49 15 Z M62 15 H80 V85 H62 Z"
+          fill="hsl(var(--primary))"
         />
+        {/* Bite mark - circle filled with the card background color to appear as a cutout */}
+        <circle cx="75" cy="25" r="10" fill="hsl(var(--card))" />
       </svg>
-      {/* Text name removed */}
     </div>
   );
 };
