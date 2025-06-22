aa277c3501e48cd2ed4dc1c94b3ba3af1bacb307
Aniket Singh Rajput <rajnishsingh333u@gmail.com>
2025-06-22 07:35:00 +0000

    change the font style of this app is in Fantasy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Cinzel', 'serif'],
+        headline: ['Cinzel', 'serif'],
         code: ['monospace'],
       },
       colors: {
