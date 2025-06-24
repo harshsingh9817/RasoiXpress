4d5de2551e473339c24e413689911e9652366ff7
Aniket Singh Rajput <rajnishsingh333u@gmail.com>
2025-06-24 19:22:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.ts
+++ src/next.config.ts
@@ -40,6 +40,18 @@ const nextConfig: NextConfig = {
         hostname: 'assets.epicurious.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
