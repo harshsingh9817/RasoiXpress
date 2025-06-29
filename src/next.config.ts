718e4e2715302a28a348038a0a1384a57fa7fcff
Aniket Singh Rajput <rajnishsingh333u@gmail.com>
2025-06-29 15:04:46 +0000

    Error: Invalid src prop (https://images.unsplash.com/photo-1594007654729

--- src/next.config.ts
+++ src/next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
