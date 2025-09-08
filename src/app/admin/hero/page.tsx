950244a19a8b8b936ce108e0111abcdcf35c3b16
Archana Singh <harshsingh9817@gmail.com>
2025-09-08 05:45:58 +0000

    Try fixing this error: `Runtime Error: Error: Failed to construct 'Image

--- src/app/admin/hero/page.tsx
+++ src/app/admin/hero/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
